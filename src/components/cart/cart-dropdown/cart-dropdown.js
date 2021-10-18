a05819ff99f4e3dc0beceaa2f06420ba4d0523f1
Graeme Elliott <graeme.elliott@dimedout.com>
2021-10-19 00:09:32 +0100

    updating with reselect, replaced cart selectors with memoized selectors

--- src/components/cart/cart-dropdown/cart-dropdown.js
+++ src/components/cart/cart-dropdown/cart-dropdown.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import CustomButton from '../../global-components/customer-button/custom-button';
+import { selectCartItems } from "../../../redux/cart/cart.selectors";
 import './cart-dropdown.scss';
 import CartItem from "../cart-item/cart-item";
 
-const CartDropdown = ({cartItems}) => {
-    return (
-        <div className="cart-dropdown">
-            <div className="cart-items">
-                {
-                    cartItems.map(cartItem => (
-                        <CartItem key={cartItem.id} item={cartItem} />
-                    ))
-                }
-            </div>
-            <CustomButton>CHECKOUT</CustomButton>
-        </div>
-    )
-};
+const CartDropdown = ({ cartItems }) => (
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  );
+  
+  const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+  });
+  
+  export default connect(mapStateToProps)(CartDropdown);
 
-const mapStateToProps = ({cart: { cartItems }}) => ({
-    cartItems
-});
-
-export default connect(mapStateToProps) (CartDropdown);
--- src/components/cart/cart-item/cart-item.js
+++ src/components/cart/cart-item/cart-item.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './cart-item.scss';
 
-const CartItem = ({ item: {imageUrl, price, productName, quantity }}) => (
-    <div className="cart-item">
-        <img src={imageUrl} alt='item' />
-        <div className="item-details">
-            <span className="productName">{productName}</span>
-            <span className="price">{quantity} x £{price}</span>
-        </div>
+const CartItem = ({ item: { imageUrl, price, productName, quantity } }) => (
+    <div className='cart-item'>
+      <img src={imageUrl} alt='item' />
+      <div className='item-details'>
+        <span className='productName'>{productName}</span>
+        <span className='price'>
+          {quantity} x £{price}</span>
+      </div>
     </div>
-);
-
-export default CartItem;
+  );
+  
+  export default CartItem;
