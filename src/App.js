63ba06608bd9a9807b71bfc225614cef2284fc17
Graeme Elliott <graeme.elliott@dimedout.com>
2021-12-02 01:48:11 +0000

    Header and Footer included. Footer is responsive.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import './App.css';
+import Header from "./components/partials/header/header";
+import Footer from "./components/partials/footer/footer";
 
 const App = () => {
   return (
-    <div>Hello! </div>
-    
+    <BrowserRouter>
+      <Header />
+      <div className="page-container">
+        <div className="content">Hello World! </div>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/partials/header/header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/cpc-logo.jpg";
+import "./header.scss";
+
+const Header = () => (
+  <div className="header">
+    <div className="header-top-wrapper">
+      <div className="search-bar header-text"> Search </div>
+      <Link className="logo-wrapper" to="/">
+        <img className="logo-img" src={logo} alt="" />
+      </Link>
+      <div className="account-links-wrapper header-text">
+        <Link className="account-link" to="/sign-in">SIGN IN / REGISTER</Link>
+      </div>
+    </div>
+    <div className="header-bottom-wrapper">
+      <Link className="header-link" to="/products">
+        PRODUCTS
+      </Link>
+      <Link className="header-link" to="/about-us">
+        ABOUT US
+      </Link>
+      <Link className="header-link" to="/contact-us">
+        CONTACT US
+      </Link>
+    </div>
+  </div>
+);
+
+
+export default Header;
