0e459152baf299efe91adaeb80181f44a54a3e43
Graeme Elliott <graeme.elliott@dimedout.com>
2021-12-17 13:28:41 +0000

    Testing using a sandbox for remote working

--- src/components/products/product-details/product-details.component.jsx
+++ src/components/products/product-details/product-details.component.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import './product-details.component.scss';
+import "./product-details.component.scss";
+import CustomButton from "../../partials/customer-button/custom-button";
 
 const ProductDetails = () => {
-    return (
-        <div className="product-details-wrapper">
-            <div className='product-image'
+  return (
+    <div className="product-details-wrapper">
+      <div
+        className="product-image"
         style={{
-            backgroundImage: 'url("https://i.ibb.co/QfpjK7P/Lucky-Rabbit.jpg")'}}/>
+          backgroundImage: 'url("https://i.ibb.co/QfpjK7P/Lucky-Rabbit.jpg")'
+        }}
+      />
 
-            <div className="product-details" >
-                <div> Product Name with a long name like this one and then some </div>
-                <div> Brand </div>
-                <div> Price </div>
-                <div> Qty </div>
-                <div> Custom Button </div>
-                <div> Description </div>
-            </div>
-
-            
-        </div>
-    )
-}
+      <div className="product-details">
+        <div> Product Name with a long name like this one and then some </div>
+        <div> Brand </div>
+        <div> Price </div>
+        <div> Qty </div>
+        <CustomButton> Add To Cart </CustomButton>
+        <div> Description </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
